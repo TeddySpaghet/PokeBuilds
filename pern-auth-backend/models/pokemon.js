0a5825dc59544d852325c77067f496c76d95b624
theodore pochart <theodore.pochart@gmail.com>
2020-11-27 13:54:14 -0800

    created tedContext branch and set up db

---
+++ pern-auth-backend/models/pokemon.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class pokemon extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  pokemon.init({
+    name: DataTypes.STRING,
+    move0: DataTypes.STRING,
+    move1: DataTypes.STRING,
+    move2: DataTypes.STRING,
+    move3: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'pokemon',
+  });
+  return pokemon;
+};
---
+++ pern-auth-backend/models/team.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class team extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  team.init({
+    teamName: DataTypes.STRING,
+    teamDescription: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'team',
+  });
+  return team;
+};
