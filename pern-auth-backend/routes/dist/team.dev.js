01de0d974d47b81a3e0eac8e39125d4123ac2735
nickschmitt <nickschmitt@gmail.com>
2020-11-29 22:20:42 -0800

    tedContext to TeamContext merge

--- pern-auth-backend/routes/dist/team.dev.js
+++ pern-auth-backend/routes/dist/team.dev.js
@@ -5,6 +5,7 @@ var router = require('express').Router();
 var team = require('../controllers/team');
 
 router.get('/', team.index);
+router.get('/user/:id', team.indexByUser);
 router.get('/:id', team.show);
 router.post('/', team.create);
 router.put('/:id', team.update);
