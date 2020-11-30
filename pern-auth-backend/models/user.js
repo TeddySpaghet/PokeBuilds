51164ace34c96384b5ad8b09576eb28db5803a82
theodore pochart <theodore.pochart@gmail.com>
2020-11-30 09:24:17 -0800

    merging

--- pern-auth-backend/models/user.js
+++ pern-auth-backend/models/user.js
@@ -14,6 +14,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      models.user.hasMany(models.team)
     }
     validPassword(passwordTyped) {
       return bcrypt.compareSync(passwordTyped, this.password);
