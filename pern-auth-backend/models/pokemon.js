12ba66014e9d8c8fbbe8b0a2c1a26185e910306c
theodore pochart <theodore.pochart@gmail.com>
2020-11-29 16:55:55 -0800

    databse changes

--- pern-auth-backend/models/pokemon.js
+++ pern-auth-backend/models/pokemon.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      models.pokemon.belongsTo(models.team)
     }
   }
   pokemon.init(
