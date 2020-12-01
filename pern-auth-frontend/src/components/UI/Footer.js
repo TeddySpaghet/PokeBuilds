bcafd2850f721e047494336d9c5b033a06200e54
theodore pochart <theodore.pochart@gmail.com>
2020-11-30 16:37:31 -0800

    fixed styling so that pokecontainers are better lined up

--- pern-auth-frontend/src/components/UI/Footer.js
+++ pern-auth-frontend/src/components/UI/Footer.js
@@ -4,7 +4,7 @@ import './Footer.scss'
 const Footer = () => {
   return (
     <div className='footer'>
-      <hr className='fancy-line' />
+      {/* <hr className='fancy-line' /> */}
       <p>Created by Nick Schmitt, Theodore Pochart, and Nile Ausar.</p>
     </div>
   )
